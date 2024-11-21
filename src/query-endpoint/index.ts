e0a39f936431548eade871ede92b8edfdd6806fb
Joshua Bemenderfer <josh@thederf.com>
2024-11-21 09:23:08 -0500

    Attempt to support multiple queries

--- src/query-endpoint/index.ts
+++ src/query-endpoint/index.ts
@@ -1,50 +1,55 @@
-import { defineEndpoint } from '@directus/extensions-sdk'
+import { defineEndpoint } from "@directus/extensions-sdk";
 
 export default defineEndpoint((router, { database }) => {
-  router.post('/', async (req, res) => {
+  router.post("/", async (req, res) => {
     // Do not allow the user to execute queries unless they are an admin or have CRUD permissions on the query table.
-    let mayProceed = req?.accountability?.admin || false
+    let mayProceed = req?.accountability?.admin || false;
 
     if (!mayProceed && req?.accountability?.permissions) {
-      const permissions = req?.accountability?.permissions
+      const permissions = req?.accountability?.permissions;
 
       const mayCreate = permissions.some(
-        (p) => p.collection === 'query' && p.action === 'create',
-      )
+        (p) => p.collection === "query" && p.action === "create"
+      );
       const mayRead = permissions.some(
-        (p) => p.collection === 'query' && p.action === 'read',
-      )
+        (p) => p.collection === "query" && p.action === "read"
+      );
 
       const mayUpdate = permissions.some(
-        (p) => p.collection === 'query' && p.action === 'update',
-      )
+        (p) => p.collection === "query" && p.action === "update"
+      );
 
       const mayDelete = permissions.some(
-        (p) => p.collection === 'query' && p.action === 'delete',
-      )
+        (p) => p.collection === "query" && p.action === "delete"
+      );
 
-      mayProceed = mayCreate && mayRead && mayUpdate && mayDelete
+      mayProceed = mayCreate && mayRead && mayUpdate && mayDelete;
     }
 
-    if (!mayProceed) throw new Error('Permission denied')
+    if (!mayProceed) throw new Error("Permission denied");
 
     try {
       // Execute the query
-      const { query, parameters } = req.body
-      if (!query) throw new Error('No query specified')
-      const data = await database.raw(query, parameters || {})
+      const { query, parameters } = req.body;
+      if (!query) throw new Error("No query specified");
 
-      res.setHeader('Content-Type', 'application/json')
-      res.status(200)
-      res.end(JSON.stringify({ data }))
+      const queries = query.split(";\n");
+      let data = null;
+      for (const query of queries) {
+        data = await database.raw(query, parameters || {});
+      }
+
+      res.setHeader("Content-Type", "application/json");
+      res.status(200);
+      res.end(JSON.stringify({ data }));
     } catch (e: any) {
-      res.setHeader('Content-Type', 'application/json')
-      res.status(400)
+      res.setHeader("Content-Type", "application/json");
+      res.status(400);
       res.end(
         JSON.stringify({
-          error: { message: e.message, extensions: { code: 'Bad Request' } },
-        }),
-      )
+          error: { message: e.message, extensions: { code: "Bad Request" } },
+        })
+      );
     }
-  })
-})
+  });
+});
